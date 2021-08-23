5acd5a9810d4d1103881b1887af0d98dbb859648
L2Eme <yume.site@outlook.com>
2021-08-23 16:48:58 +0800

    add an index file

---
+++ youtube/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const YoutubeAuth_1 = require("./YoutubeAuth");
+exports.YoutubeAuth = YoutubeAuth_1.YoutubeAuth;
+const YoutubeEnd_1 = require("./YoutubeEnd");
+exports.YoutubeEnd = YoutubeEnd_1.YoutubeEnd;
+exports.youtube_v3 = YoutubeEnd_1.youtube_v3;
---
+++ youtube/index.ts
@@ -0,0 +1,6 @@
+import { YoutubeAuth } from './YoutubeAuth'
+import { YoutubeEnd, youtube_v3 } from './YoutubeEnd'
+
+export {
+	YoutubeAuth, YoutubeEnd, youtube_v3,
+}
